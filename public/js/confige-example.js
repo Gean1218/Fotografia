8adbe47ce603bf4ea9e19f6871cd0782dc2d2c71
Gean Marques <geanmarquesdearaujo@gmail.com>
2025-06-17 10:04:23 -0300

    atualizando projeto

---
+++ public/js/confige-example.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID",
+  measurementId: "SEU_MEASUREMENT_ID"
+};
+
+const googleDriveConfig = {
+    apiKey:"Sua_API_KEY_DO_GOOGLE_DRIVE",
+    folderID:"SEU_ID_DE_PASTA_DO_GOOGLE_DRIVE"
+}
